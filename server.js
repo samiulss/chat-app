c127f1c7908907dfa348dceb90999157d56388d6
samiulss <works.samiul@gmail.com>
2023-08-25 01:15:51 +0600

    link updated

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const server = app.listen(PORT, console.log(`Server is live at PORT ${PORT}`));
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:5173"
+        origin: "https://chat-application-dtls.onrender.com"
     },
 });
 
